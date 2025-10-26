753e648e297d30fe5c29f80bc6bc1efbd4dabbb8
Mr.Igor <iv_mirsoft@ukr.net>
2025-10-26 18:57:22 +0200

    api base url

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,7 +3,8 @@ import axios, { AxiosError } from "axios"
 export type ApiError = AxiosError<{ error: string }>
 
 export const api = axios.create({
-	//baseURL: "https://notehub-api.goit.study",
-	baseURL: "http://localhost:3030",
+	//baseURL: "https://notehub-api.goit.study",https://zero1-express.onrender.com
+	//baseURL: "http://localhost:3030",
+	baseURL: "https://zero1-express.onrender.com",
 	withCredentials: true,
 })
